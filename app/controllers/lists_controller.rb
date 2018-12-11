66aa983b9815785ea780de8d8e3d05f91304e498
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-12-11 18:45:12 +0000

    Form to add list not finished, missing tags input

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,21 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.user = current_user
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:list_type, :name, :photo, :description)
+  end
 end
