8a4a2c0a77af9a53d52db3a71abace2f91883604
Paul Busby <paul.busby@gmail.com>
2018-12-11 18:44:22 +0000

    Connected models to rough html outline

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,16 @@
 class ListsController < ApplicationController
+
+
+
+
+
+
+
+  def show
+    @list = List.find(params[:id])
+    @listtags = ListTag.where(list_id: @list)
+    @items = Item.where(list_id: @list)
+
+  end
+
 end
