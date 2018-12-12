2b58aad3cf341e8667e2af0d2fdc3072bc2e8caf
Paul Busby <paul.busby@gmail.com>
2018-12-12 17:08:38 +0000

    Added the comments crud and displayed them

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  def index
+    @comment = Comment.new
+    @comments = Comment.all
+  end
+
+  def create
+
+    @comment = Comment.new(comment_params)
+    @list = List.find(params[:list_id])
+    @item = Item.find(params[:item_id])
+    @comment.item = @item
+    @comment.user = current_user
+    @comment.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:description, :item_id, :user_id)
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,9 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     @listtags = ListTag.where(list_id: @list)
     @items = Item.where(list_id: @list)
+    @comment = Comment.new
+    # @user = current_user
+    # @comments = Comment.where(user_id: @user, item_id: @item)
   end
 
   def destroy
