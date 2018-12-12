75e571be3c24872e6b2f754994890920e6807c4d
Paul Busby <paul.busby@gmail.com>
2018-12-12 13:26:41 +0000

    Finished crud for lists, still working on items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,41 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @list = List.find(params[:list_id])
+    @item.list = @list
+    if @item.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @list = @item.list
+    @item.destroy
+    redirect_to list_path(@list)
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :photo, :description, :list_id)
+  end
+
 end
