aac14c9d348ace59a21da4e61bb21d33dd1e09b6
Paul Busby <paul.busby@gmail.com>
2018-12-12 18:06:49 +0000

    Fixed the modals

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
 import "bootstrap";
 import './application.css'
 import {select} from '../components/select';
+import {megamodal} from '../components/modal';
+megamodal();
 select();
-// import {horizontalScrollingBanner, setUpElements, scrollTheBanners} from "../components/home";
-// horizontalScrollingBanner();
-// setUpElements();
-// scrollTheBanners();
+import {horizontalScrollingBanner, setUpElements, scrollTheBanners} from "../components/home";
+horizontalScrollingBanner();
+setUpElements();
+scrollTheBanners();
