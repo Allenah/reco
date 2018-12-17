cb8a2f358321bb469d79b8c27ffa95ebc6c726e3
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-12-17 10:52:39 +0000

    Added more seeds with users, movies paul and albums

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :list
-  belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :users, :through => :likes
   has_many :comments, dependent: :destroy
