6eb436ac3b13c0b522a8b3ce2e93854e247b3e6b
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-12-17 13:18:43 +0000

    Created Users controller to display friends profile page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,9 +2,4 @@ class FriendshipsController < ApplicationController
   def index
     @friends = current_user.friends
   end
-
-  def show
-    @friendship = Friendship.find(params[:id])
-    @friend = User.find_by_id(@friendship.friend_id)
-  end
 end
