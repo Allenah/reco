ff59ae52d0dbcbf3fdc846938131a79d6e900e85
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-12-18 19:05:34 +0000

    worked on orders by follows, bookmarks, recent created

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,14 +2,14 @@ class ListsController < ApplicationController
   def index
     if params[:query].present?
       @lists = List.search_by_tag_and_friend(params[:query])
+      @lists = @lists.reverse
     else
       @follows = Follow.all
-      @lists_followed = @follows.where(user_id: current_user.id)
+      @lists_followed = @follows.where(user_id: current_user.id).order('follows.created_at').reverse
       @lists = []
       @lists_followed.each do |f|
         @lists << f.list
       end
-      return @lists
     end
   end
 
@@ -18,9 +18,15 @@ class ListsController < ApplicationController
   end
 
   def create
+
     @list = List.new(list_params)
+
     @list.user = current_user
     if @list.save
+      results = params["list"]["tag_ids"]
+      results.each do |r|
+        ListTag.create(tag_id: r, list_id: @list.id)
+      end
       redirect_to list_path(@list)
     else
       render :new
