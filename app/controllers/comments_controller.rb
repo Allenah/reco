671b20faf492bb70b7e0f303a93fcde6d60d6d29
Carolina Lemos <carolinaperestrelodelemos@gmail.com>
2018-12-19 18:33:58 +0000

    comments not fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :js, :json, :html
   def index
     @comment = Comment.new
     @comments = Comment.all
@@ -9,10 +10,11 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @list = List.find(params[:list_id])
     @item = Item.find(params[:item_id])
+    @listtags = @list.listTags
     @comment.item = @item
     @comment.user = current_user
     @comment.save
-    redirect_to list_path(@list)
+    #redirect_to list_path(@list)
   end
 
   private
